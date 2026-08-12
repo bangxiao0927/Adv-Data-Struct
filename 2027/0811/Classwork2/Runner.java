e2bbcc97760e820e84a54b51c069f403b952f8c8
Bang Xiao <bangxiao0927@gmail.com>
2026-08-11 17:37:41 -0700

    Add Runner class to create and display a JFrame for the calculator

---
+++ 2027/0811/Classwork2/Runner.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Runner {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 700);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
