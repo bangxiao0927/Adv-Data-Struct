08562b2cbe872c37d5923044ed1cbdd0e7603bb1
Bang Xiao <bangxiao0927@gmail.com>
2026-08-24 15:14:42 -0700

    Refactor GameManager and Runner classes for improved game logic and user interaction

--- 2027/0818/Classwork2/GameManager.java
+++ 2027/0818/Classwork2/GameManager.java
@@ -1,10 +1,11 @@
-public class GameManager{
-    private String[][] grid = new String[3][3];
+public class GameManager {
+    private static final String EMPTY = "*";
+    private final String[][] grid = new String[3][3];
 
     public GameManager(){
         for (int i = 0; i < grid.length; i++){
             for (int j = 0; j < grid[i].length; j++){
-                grid[i][j] = "*";
+                grid[i][j] = EMPTY;
             }
         }
     }
@@ -12,14 +13,14 @@ public class GameManager{
     public void resetGrid() {
         for (int i = 0; i < grid.length; i++){
             for (int j = 0; j < grid[i].length; j++){
-                grid[i][j] = "*";
+                grid[i][j] = EMPTY;
             }
         }
     }
 
     public void printGrid() {
         for(int i = 0 ; i < this.grid.length; i++) {
-            for(int j = 0; j < this.grid.length; j++) {
+            for(int j = 0; j < this.grid[i].length; j++) {
                 System.out.print(this.grid[i][j]);
             }
             System.out.print("\n");
@@ -27,80 +28,70 @@ public class GameManager{
     }
 
     public boolean insertX(int row, int col){
-        if (grid[row-1][col-1].equals("*")){
-            grid[row-1][col-1] = "X";
+        return insert(row, col, "X");
+    }
+
+    public boolean insertO(int row, int col){
+        return insert(row, col, "O");
+    }
+
+    private boolean insert(int row, int col, String mark) {
+        if (row < 1 || row > grid.length || col < 1 || col > grid[0].length) {
+            return false;
+        }
+
+        if (grid[row - 1][col - 1].equals(EMPTY)) {
+            grid[row - 1][col - 1] = mark;
             return true;
         }
         return false;
     }
 
-    public boolean insertO(int row, int col){
-        if (grid[row-1][col-1].equals("*")){
-            grid[row-1][col-1] = "O";
-            return true;
-        }
-        return false;     
-    }
-
     public boolean checkWin() {
-        // diagnal
         for (int i = 0; i < grid.length; i++) {
-            if (grid[i][0] != "*"
-                    && grid[i][0] == grid[i][1]
-                    && grid[i][1] == grid[i][2]) {
+            if (!grid[i][0].equals(EMPTY)
+                    && grid[i][0].equals(grid[i][1])
+                    && grid[i][1].equals(grid[i][2])) {
                 return true;
             }
 
-            if (grid[0][i] != "*"
-                    && grid[0][i] == grid[1][i]
-                    && grid[1][i] == grid[2][i]) {
+            if (!grid[0][i].equals(EMPTY)
+                    && grid[0][i].equals(grid[1][i])
+                    && grid[1][i].equals(grid[2][i])) {
                 return true;
             }
         }
 
-        // start from origin
-        if (grid[0][0] != "*"
-                && grid[0][0] == grid[1][1]
-                && grid[1][1] == grid[2][2]) {
+        if (!grid[0][0].equals(EMPTY)
+                && grid[0][0].equals(grid[1][1])
+                && grid[1][1].equals(grid[2][2])) {
             return true;
         }
 
-        // start from then end corner
-        return grid[0][2] != "*"
-                && grid[0][2] == grid[1][1]
-                && grid[1][1] == grid[2][0];
+        return !grid[0][2].equals(EMPTY)
+                && grid[0][2].equals(grid[1][1])
+                && grid[1][1].equals(grid[2][0]);
     }
 
-    public void computerTurn(){
+    public boolean isFull() {
+        for (String[] row : grid) {
+            for (String cell : row) {
+                if (cell.equals(EMPTY)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void computerTurn() {
         for (int i = 0; i < grid.length; i++){
             for (int j = 0; j < grid[i].length; j++){
-                if (grid[i][j].equals("*")){
-                    insertO(i,j);
-                    i = grid.length;
-                    j = grid[i].length;
+                if (grid[i][j].equals(EMPTY)){
+                    insertO(i + 1, j + 1);
+                    return;
                 }
             }
         }
     }
-
-    // WOWWWOWO
-    // public boolean checkWin() {
-    //     return checkWinHelper(0, 0, 0);
-    // }
-
-    // public boolean checkWinHelper(int row, int col, int type) {
-    //     if ((row == 0 && col == 2) || (row == 2 && col == 0) || (row == 2 && col == 2)) {
-    //         if (type == 1) {
-    //             return true;
-    //         } else if (type == 0) {
-    //             return false;
-    //         }
-    //     }
-
-    //     boolean a = checkWinHelper(row+1, col, type);
-    //     boolean b = checkWinHelper(row, col+1, type);
-
-    //     boolean result = a || b;
-    //     return result;
-    // }
 }
--- 2027/0818/Classwork2/Runner.java
+++ 2027/0818/Classwork2/Runner.java
@@ -6,25 +6,55 @@ public class Runner {
 
         Scanner sc = new Scanner(System.in);
 
-        boolean over = false;
         int turn = 1;
         System.out.println("Tic-Tac-Toe: You will first enter the row of the position you want to select, then the column.");
 
-        while (!over){
-
+        while (true) {
             gm.printGrid();
 
-            if (turn == 1){
+            if (turn == 1) {
                 System.out.println("To select a position, enter the number of the row (1 is the top)");
-                int row = sc.nextInt();
+                int row = readNumber(sc);
                 System.out.println("Now enter the column (1 is left)");
-                int col = sc.nextInt();
-                gm.insertX(row, col);
+                int col = readNumber(sc);
+
+                if (!gm.insertX(row, col)) {
+                    System.out.println("That position is invalid or already occupied. Try again.");
+                    continue;
+                }
+
+                if (gm.checkWin()) {
+                    gm.printGrid();
+                    System.out.println("You win!");
+                    break;
+                }
                 turn = 2;
-            } else{
+            } else {
                 gm.computerTurn();
+
+                if (gm.checkWin()) {
+                    gm.printGrid();
+                    System.out.println("The computer wins!");
+                    break;
+                }
                 turn = 1;
             }
+
+            if (gm.isFull()) {
+                gm.printGrid();
+                System.out.println("It's a draw!");
+                break;
+            }
         }
+
+        sc.close();
+    }
+
+    private static int readNumber(Scanner sc) {
+        while (!sc.hasNextInt()) {
+            System.out.println("Please enter a whole number.");
+            sc.next();
+        }
+        return sc.nextInt();
     }
 }
