910835697f670f3a71b5cfc4ec429c3952253f26
Bang Xiao <bangxiao0927@gmail.com>
2026-08-25 20:26:04 -0700

    Add food model

---
+++ 2027/0824/Classwork/Food.java
@@ -0,0 +1,19 @@
+public class Food {
+// Food Class
+// Instance Variables
+// 	- name : String - Food name.
+// Constructor
+// 	+ Food(String) - Sets up the instance variable accordingly.
+// Methods
+// 	+ toString() : String - Return the name.
+
+    private String name;
+
+    public Food(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
