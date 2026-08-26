d81131886ae486431ad7a74c939cfa794d8bd066
Bang Xiao <bangxiao0927@gmail.com>
2026-08-25 20:26:04 -0700

    Add generic item model

---
+++ 2027/0824/Classwork/Item.java
@@ -0,0 +1,17 @@
+public class Item<T>{
+    private T o;
+    private double price;
+
+    public Item(T o, double price){
+        this.o = o;
+        this.price = price;
+    }
+
+    public T getItem(){
+        return o;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+}
