77886200292e43c0a6aa030624f127a32a2ec479
Bang Xiao <bangxiao0927@gmail.com>
2026-08-25 20:26:04 -0700

    Add cat costume

---
+++ 2027/0820/Classwork/Cat.java
@@ -0,0 +1,26 @@
+import java.awt.Graphics;
+
+public class Cat extends Costume {
+    public Cat(int x, int y) {
+        super(x, y);
+    }
+
+    public void drawNose(Graphics g) {
+        int[] xPoints = {getX() + 20, getX() + 15, getX() + 25};
+        int[] yPoints = {getY() + 18, getY() + 23, getY() + 23};
+        g.drawPolygon(xPoints, yPoints, 3);
+
+        g.drawLine(getX() + 20, getY() + 23, getX() + 30, getY() + 28);
+        g.drawLine(getX() + 20, getY() + 23, getX() + 10, getY() + 28);
+    }
+  
+    public void drawEars(Graphics g) {
+        int[] xPoints1 = {getX() + 10, getX() + 5, getX() + 15};
+        int[] yPoints1 = {getY(), getY() - 15, getY() - 10};
+        g.drawPolygon(xPoints1, yPoints1, 3);
+    
+        int[] xPoints2 = {getX() + 30, getX() + 25, getX() + 35};
+        int[] yPoints2 = {getY(), getY() - 10, getY() - 15};
+        g.drawPolygon(xPoints2, yPoints2, 3);
+    }
+}
