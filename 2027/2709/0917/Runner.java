e7b41b26a82c1f7403eb8d6afce4552f354d75b4
Ayla <atestic13@gmail.com>
2026-09-17 14:35:42 -0700

    Fix method calls for setting list data in Runner class

--- 2027/2709/0917/Runner.java
+++ 2027/2709/0917/Runner.java
@@ -12,21 +12,24 @@ public class Runner{
                 for (int i = 0; i < list.size(); i++) {
                     int randomIndex = (int) (Math.random() * list.size());
                     int temp = list.get(i);
-                    list.setData(i, list.get(randomIndex));
-                    list.setData(randomIndex, temp);
+                    list.set(i, list.get(randomIndex));
+                    list.set(randomIndex, temp);
                 }
             } else if (choice == 3){
                 for (int pass = 0; pass < list.size() - 1; pass++) {
                     for (int i = 0; i < list.size() - 1 - pass; i++) {
                         if (list.get(i) > list.get(i + 1)) {
                             int temp = list.get(i);
-                            list.setData(i, list.get(i + 1));
-                            list.setData(i + 1, temp);
+                            list.set(i, list.get(i + 1));
+                            list.set(i + 1, temp);
                         }
                     }
                 }
             } else if (choice == 4){
-
+                while (list.size() > 0){
+                    int rand = (int) (Math.random() * list.size());
+                    
+                }
             } else{
 
             }
