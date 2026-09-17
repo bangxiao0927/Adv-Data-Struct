34fd8941dded7000ab70fef48faa54de54764965
Ayla <atestic13@gmail.com>
2026-09-17 14:32:00 -0700

    Implement main method logic for user interaction and list manipulation

--- 2027/2709/0917/Runner.java
+++ 2027/2709/0917/Runner.java
@@ -1,5 +1,35 @@
 public class Runner{
     public static void main(String[] args){
-        
+
+
+
+        boolean stop = false;
+
+        while(!stop){
+            if (choice == 1){
+                System.out.println(list.toString());
+            } else if (choice == 2){
+                for (int i = 0; i < list.size(); i++) {
+                    int randomIndex = (int) (Math.random() * list.size());
+                    int temp = list.get(i);
+                    list.setData(i, list.get(randomIndex));
+                    list.setData(randomIndex, temp);
+                }
+            } else if (choice == 3){
+                for (int pass = 0; pass < list.size() - 1; pass++) {
+                    for (int i = 0; i < list.size() - 1 - pass; i++) {
+                        if (list.get(i) > list.get(i + 1)) {
+                            int temp = list.get(i);
+                            list.setData(i, list.get(i + 1));
+                            list.setData(i + 1, temp);
+                        }
+                    }
+                }
+            } else if (choice == 4){
+
+            } else{
+
+            }
+        }
     }
 }
