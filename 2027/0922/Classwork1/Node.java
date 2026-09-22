f46a07b6beaa1ed1dcc6ffe7a94de5afb5a0751a
Ayla <atestic13@gmail.com>
2026-09-22 14:44:34 -0700

    Add Node class for doubly linked list implementation

---
+++ 2027/0922/Classwork1/Node.java
@@ -0,0 +1,31 @@
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+    private Node<E> prev;
+
+    public Node(E ele){
+        data = ele;
+        next = null;
+        prev = null;
+    }
+
+    public E get(){
+        return data;
+    }
+
+    public Node<E> next(){
+        return next;
+    }
+
+    public Node<E> prev(){
+        return prev;
+    }
+
+    public void setNext(Node<E> newNode){
+        next = newNode;
+    }
+
+    public void setPrev(Node<E> newNode){
+        prev = newNode;
+    }
+}
